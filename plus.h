8b964df374e697650605fa87e72b96674ca8443a
Matthew Gregorek <132983612+Mgrego12@users.noreply.github.com>
2024-01-09 16:24:10 -0500

    Create plus.h

---
+++ plus.h
@@ -0,0 +1,8 @@
+class Plus: public SubExpression {
+public:
+    Plus(Expression* left, Expression* right): SubExpression(left, right) {
+    }
+    double evaluate()  {
+       return left->evaluate() + right->evaluate();
+    }
+};
